40b3ba576ddc7c0f2f69a68f8f38c3bfdf70c69f
Android Studio <devandroid@ort>
2026-06-12 08:14:31 -0300

    arreglos

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 const DBConfig = {
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_DATABASE || 'asd',
+  database: process.env.DB_DATABASE || 'postgres',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'root',
   port: parseInt(process.env.DB_PORT || '5432', 10),
