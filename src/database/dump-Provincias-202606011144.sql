5fb467d4ef1c2dc8d1c13c986e2d78fe2800a27a
SANTIAGO NAHUEL CHOE <49092487@ort>
2026-06-12 08:04:22 -0300

    database

---
+++ src/database/dump-Provincias-202606011144.sql
@@ -0,0 +1,90 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.2
+-- Dumped by pg_dump version 16.2
+CREATE DATABASE asd WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Spanish_Argentina.1252';
+-- Started on 2026-06-01 11:44:09
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 216 (class 1259 OID 16400)
+-- Name: provincias; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.provincias (
+    id integer NOT NULL,
+    name character varying NOT NULL,
+    full_name character varying NOT NULL,
+    latitude integer,
+    longtitude integer,
+    display_order integer
+);
+
+
+ALTER TABLE public.provincias OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 16399)
+-- Name: provincias_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.provincias ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.provincias_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 4780 (class 0 OID 16400)
+-- Dependencies: 216
+-- Data for Name: provincias; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.provincias (id, name, full_name, latitude, longtitude, display_order) FROM stdin;
+\.
+
+
+--
+-- TOC entry 4786 (class 0 OID 0)
+-- Dependencies: 215
+-- Name: provincias_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.provincias_id_seq', 1, false);
+
+
+--
+-- TOC entry 4635 (class 2606 OID 16406)
+-- Name: provincias provincias_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.provincias
+    ADD CONSTRAINT provincias_pk PRIMARY KEY (id);
+
+
+-- Completed on 2026-06-01 11:44:09
+
+--
+-- PostgreSQL database dump complete
+--
+
